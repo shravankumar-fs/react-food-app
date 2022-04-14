c1175ed8f2de9333a27740b410edc1b942d3f76c
star dragon <59759431+stardragoncoder@users.noreply.github.com>
2022-04-14 18:25:23 +0530

    Partial -MEalItem additions

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,5 @@
 import classes from './MealItem.module.css';
-
+import MealItemForm from './MealItemForm';
 const MealItem = (props) => {
   const price = `$${props.item.price.toFixed(2)}`;
 
@@ -10,7 +10,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.item.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm id={props.item.id} />
+      </div>
     </li>
   );
 };
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,5 +1,21 @@
 import classes from './MealItemForm.module.css';
-
-const MealItemForm = (props) => {};
+import Input from '../../UI/Input';
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.formClass}>
+      <Input
+        label='Amount'
+        input={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          defaultValue: '1',
+        }}
+      />
+      <button> + Add</button>
+    </form>
+  );
+};
 
 export default MealItemForm;
