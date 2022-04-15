e7026cca69ef8a2a86080d05983e7f8cd4934ebe
star dragon <59759431+stardragoncoder@users.noreply.github.com>
2022-04-15 08:11:24 +0530

    basic Modal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Cart></Cart>
       <main>
         <Meals />
       </main>
